48e94ccc1b5147b179c958d8161a3e4e7ad64926
CarlosMBarboza <michaelalexander81@hotmail.com>
2024-10-25 14:38:14 -0300

    cambio de rutas

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -2,49 +2,49 @@ export const ProjectsList = [
   {
     name: "Contador de Calorias",
     link: "https://calorie-tracker-6e4j-9po6hq5tf-carlosmbarbozas-projects.vercel.app/",
-    image: "/src/images/caloria.png",
+    image: "../../src/images/caloria.png",
     description: "Una aplicación interactiva desarrollada en React y TypeScript que permite a los usuarios llevar un registro de su ingesta calórica diaria de manera sencilla y efectiva."
   },
   {
     name: "Miusicenter",
     link: "https://miusicenter-typescript-aamnnbzmh-carlosmbarbozas-projects.vercel.app/",
-    image: "/src/images/miusicenter.png",
+    image: "../../src/images/miusicenter.png",
     description: "Plataforma musical construida con React y TypeScript, diseñada para ofrecer a los usuarios una experiencia intuitiva al explorar y disfrutar de su música favorita."
   },
   {
     name: "Calcular Descuento",
     link: "https://calcular-de-descuentos.vercel.app/",
-    image: "/src/images/CalcularDescuento.png",
+    image: "../../src/images/CalcularDescuento.png",
     description: "Herramienta útil en React y TypeScript que permite a los usuarios calcular descuentos de manera rápida y fácil, facilitando la toma de decisiones de compra."
   },
   {
     name: "Buscador de Gifs",
     link: "https://gifs-app-self.vercel.app/",
-    image: "/src/images/BuscadorGifs.png",
+    image: "../../src/images/BuscadorGifs.png",
     description: "Aplicación desarrollada en Angular y TypeScript que permite a los usuarios buscar y compartir gifs de manera ágil, haciendo uso de una API de gifs popular."
   },
   {
     name: "Anotador de Tareas",
     link: "https://primer-anotador-tareas.netlify.app/",
-    image: "/src/images/anotador.png",
+    image: "../../src/images/anotador.png",
     description: "Aplicación de gestión de tareas construida con React y TypeScript, que ayuda a los usuarios a organizar y priorizar sus actividades diarias de forma eficiente."
   },
   {
     name: "Turnos Barberia",
     link: "https://turno-barberia.netlify.app",
-    image: "/src/images/barberia.png",
+    image: "../../src/images/barberia.png",
     description: "Sistema de gestión de citas para barberías, desarrollado en React, que permite a los usuarios reservar turnos de manera sencilla y rápida."
   },
   {
     name: "Listado de Tareas",
     link: "https://angular-primeros-pasos.vercel.app/",
-    image: "/src/images/listado.png",
+    image: "../../src/images/listado.png",
     description: "Aplicación Angular y TypeScript que permite a los usuarios crear, visualizar y gestionar un listado de tareas, mejorando su productividad."
   },
   {
     name: "Buscador de Paises",
     link: "https://country-app-sepia-nine.vercel.app/countries/by-capital",
-    image: "/src/images/paises.png",
+    image: "../../src/images/paises.png",
     description: "Aplicación interactiva en Angular y TypeScript que permite a los usuarios buscar información sobre países utilizando sus capitales, proporcionando datos relevantes y actualizados."
   },
 ];
