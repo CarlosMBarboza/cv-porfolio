e598c7eb0a2f27b5fae0895e09e61c41cf5fb6cc
CarlosMBarboza <michaelalexander81@hotmail.com>
2024-11-10 11:36:00 -0300

    img

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -1,50 +1,59 @@
+import Calorias from "../../src/images/caloria.png";
+import Miusicenter from "../../src/images/miusicenter.png";
+import CalcularDescuento from "../../src/images/CalcularDescuento.png";
+import BuscadorGifs from "../../src/images/BuscadorGifs.png";
+import Anotador from "../../src/images/anotador.png";
+import Barberia from "../../src/images/barberia.png";
+import Listado from "../../src/images/listado.png";
+import Paises from "../../src/images/paises.png";
+
 export const ProjectsList = [
   {
     name: "Contador de Calorias",
     link: "https://calorie-tracker-6e4j-9po6hq5tf-carlosmbarbozas-projects.vercel.app/",
-    image: "../../src/images/caloria.png",
+    image: Calorias,
     description: "Una aplicación interactiva desarrollada en React y TypeScript que permite a los usuarios llevar un registro de su ingesta calórica diaria de manera sencilla y efectiva."
   },
   {
     name: "Miusicenter",
     link: "https://miusicenter-typescript-aamnnbzmh-carlosmbarbozas-projects.vercel.app/",
-    image: "../../src/images/miusicenter.png",
+    image: Miusicenter,
     description: "Plataforma musical construida con React y TypeScript, diseñada para ofrecer a los usuarios una experiencia intuitiva al explorar y disfrutar de su música favorita."
   },
   {
     name: "Calcular Descuento",
     link: "https://calcular-de-descuentos.vercel.app/",
-    image: "../../src/images/CalcularDescuento.png",
+    image: CalcularDescuento,
     description: "Herramienta útil en React y TypeScript que permite a los usuarios calcular descuentos de manera rápida y fácil, facilitando la toma de decisiones de compra."
   },
   {
     name: "Buscador de Gifs",
     link: "https://gifs-app-self.vercel.app/",
-    image: "../../src/images/BuscadorGifs.png",
+    image: BuscadorGifs,
     description: "Aplicación desarrollada en Angular y TypeScript que permite a los usuarios buscar y compartir gifs de manera ágil, haciendo uso de una API de gifs popular."
   },
   {
     name: "Anotador de Tareas",
     link: "https://primer-anotador-tareas.netlify.app/",
-    image: "../../src/images/anotador.png",
+    image: Anotador,
     description: "Aplicación de gestión de tareas construida con React y TypeScript, que ayuda a los usuarios a organizar y priorizar sus actividades diarias de forma eficiente."
   },
   {
-    name: "Turnos Barberia",
+    name: "Turnos Barbería",
     link: "https://turno-barberia.netlify.app",
-    image: "../../src/images/barberia.png",
+    image: Barberia,
     description: "Sistema de gestión de citas para barberías, desarrollado en React, que permite a los usuarios reservar turnos de manera sencilla y rápida."
   },
   {
     name: "Listado de Tareas",
     link: "https://angular-primeros-pasos.vercel.app/",
-    image: "../../src/images/listado.png",
+    image: Listado,
     description: "Aplicación Angular y TypeScript que permite a los usuarios crear, visualizar y gestionar un listado de tareas, mejorando su productividad."
   },
   {
-    name: "Buscador de Paises",
+    name: "Buscador de País",
     link: "https://country-app-sepia-nine.vercel.app/countries/by-capital",
-    image: "../../src/images/paises.png",
+    image: Paises,
     description: "Aplicación interactiva en Angular y TypeScript que permite a los usuarios buscar información sobre países utilizando sus capitales, proporcionando datos relevantes y actualizados."
   },
 ];
